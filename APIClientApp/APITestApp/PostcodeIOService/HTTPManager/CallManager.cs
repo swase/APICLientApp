7736ba2ce1376fc0f957f0c6e988fca86979608c
swase <fg2g15@soton.ac.uk>
2021-09-16 14:56:05 +0100

    added tests for starlink requests

--- APIClientApp/APITestApp/PostcodeIOService/HTTPManager/CallManager.cs
+++ APIClientApp/APITestApp/PostcodeIOService/HTTPManager/CallManager.cs
@@ -1,9 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
 using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace APITestApp.PostcodeIOService.HTTPManager
